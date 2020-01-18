6714cda7a74fa7176aeaf3bd18d56b9b6e43461b
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2020-01-18 03:34:16 +0000

    Update test.cpp
    Updated the errors and made the tests pass for linux environment

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,8 +1,20 @@
 #define BOOST_TEST_MODULE test
 #include <boost/test/unit_test.hpp>
-#include "GDML.h"
+#include "../GDML/GDML/GDML.h"
 
-BOOST_AUTO_TEST_CASE(testFoo){
+BOOST_AUTO_TEST_CASE(basic_parser_test){
 
-BOOST_CHECK(1==1);
+    std::string test_output_string="";
+    auto basic_test_func=[&](gml::GDMLParser*parser,std::string tag,std::string value, std::string data){
+        test_output_string=data;
+        return true;};
+    
+    gml::GMLTokenCard card;
+    gml::GDMLParser parser;
+    
+    card.tie("print").with(basic_test_func);
+    parser.exec("[print:]Hello[/]",card);
+    
+    BOOST_CHECK(test_output_string=="Hello");
+
 }
