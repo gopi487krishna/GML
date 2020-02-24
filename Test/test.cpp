c185da0fa71dcf6f1d228c99864dbf7243570a79
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2020-02-24 19:06:08 +0530

    Update test.cpp

--- Test/test.cpp
+++ Test/test.cpp
@@ -10,7 +10,7 @@ BOOST_AUTO_TEST_CASE(basic_parser_test){
         return true;};
     
     gml::GMLTokenCard card;
-    gml::GDMLParser parser;
+    gml::GMLParser parser;
     
     card.tie("print").with(basic_test_func);
     parser.exec("[print:]Hello[/]",card);
@@ -23,7 +23,7 @@ BOOST_AUTO_TEST_CASE(object_structuring_syntax_test)
     std::string output_string="";
     std::string expected_string="roll-20056semester-6";
     
-    auto token_func=[&](gml::GDMLParser*parser,std::string tag,std::variant<std::string,std::map<std::string,std::string>> value, std::string data){
+    auto token_func=[&](gml::GMLParser*parser,std::string tag,std::variant<std::string,std::map<std::string,std::string>> value, std::string data){
       
        
         
@@ -44,7 +44,7 @@ BOOST_AUTO_TEST_CASE(object_structuring_syntax_test)
         
        
     gml::GMLTokenCard card;
-    gml::GDMLParser parser;
+    gml::GMLParser parser;
     card.tie("Student").with(token_func);
     parser.exec("[Student:{semester:6,roll:20056}]Hello[/]",card);
     BOOST_CHECK(output_string==expected_string);
